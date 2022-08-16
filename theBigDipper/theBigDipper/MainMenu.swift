397c8b50e7fc026b5a36bcf83cd14c024a15d29a
hopwesley <ws@sinoc.io>
2022-08-16 22:34:04 +0800

    init app

---
+++ theBigDipper/theBigDipper/MainMenu.swift
@@ -0,0 +1,47 @@
+//
+//  MainMenu.swift
+//  theBigDipper
+//
+//  Created by wesley on 2022/8/16.
+//
+
+
+import Cocoa
+import SwiftUI
+
+class MainMenu: NSObject {
+        let menu = NSMenu()
+        
+        func build() ->NSMenu{
+                menu.addItem(NSMenuItem.separator())
+
+                   let aboutMenuItem = NSMenuItem(
+                     title: "About KyanBar",
+                     action: #selector(about),
+                     keyEquivalent: ""
+                   )
+                   aboutMenuItem.target = self
+                   menu.addItem(aboutMenuItem)
+                // Adding a seperator
+                menu.addItem(NSMenuItem.separator())
+
+                // Adding a quit menu item
+                let quitMenuItem = NSMenuItem(
+                  title: "Quit KyanBar",
+                  action: #selector(quit),
+                  keyEquivalent: "q"
+                )
+                quitMenuItem.target = self
+                
+                menu.addItem(quitMenuItem)
+                return menu
+        }
+        
+        @objc func about(sender: NSMenuItem) {
+           NSApp.orderFrontStandardAboutPanel()
+         }
+
+         @objc func quit(sender: NSMenuItem) {
+           NSApp.terminate(self)
+         }
+}
